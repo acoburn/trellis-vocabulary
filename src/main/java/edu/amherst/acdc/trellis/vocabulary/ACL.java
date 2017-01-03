6d81096c7e1d23b678b026d528f8a11cc51cc58b
Aaron Coburn <acoburn@amherst.edu>
2017-01-03 05:32:09 -0500

    create initial vocabulary project

---
+++ src/main/java/edu/amherst/acdc/trellis/vocabulary/ACL.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2016 Amherst College
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.amherst.acdc.trellis.vocabulary;
+
+import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.rdf.api.RDF;
+import org.apache.commons.rdf.simple.SimpleRDF;
+
+/**
+ * RDF Terms from the W3C ACL Vocabulary
+ *
+ * @see <a href="https://www.w3.org/wiki/WebAccessControl">WebAccessControl Wiki</a>
+ *
+ * @author acoburn
+ */
+public class ACL {
+
+    private static RDF rdf = new SimpleRDF();
+
+    /* Namespace */
+    public static String uri = "http://www.w3.org/ns/auth/acl#";
+
+    /* Classes */
+    public static IRI Access = rdf.createIRI(uri + "Access");
+    public static IRI Append = rdf.createIRI(uri + "Append");
+    public static IRI Authorization = rdf.createIRI(uri + "Authorization");
+    public static IRI Control = rdf.createIRI(uri + "Control");
+    public static IRI Read = rdf.createIRI(uri + "Read");
+    public static IRI Write = rdf.createIRI(uri + "Write");
+
+    /* Properties */
+    public static IRI accessControl = rdf.createIRI(uri + "accessControl");
+    public static IRI accessTo = rdf.createIRI(uri + "accessTo");
+    public static IRI accessToClass = rdf.createIRI(uri + "accessToClass");
+    public static IRI agent = rdf.createIRI(uri + "agent");
+    public static IRI agentClass = rdf.createIRI(uri + "agentClass");
+    public static IRI agentGroup = rdf.createIRI(uri + "agentGroup");
+    public static IRI defaultForNew = rdf.createIRI(uri + "defaultForNew");
+    public static IRI delegates = rdf.createIRI(uri + "delegates");
+    public static IRI mode = rdf.createIRI(uri + "mode");
+    public static IRI owner = rdf.createIRI(uri + "owner");
+
+    private ACL() {
+        // prevent instantiation
+    }
+}
---
+++ src/main/java/edu/amherst/acdc/trellis/vocabulary/Fedora.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2016 Amherst College
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.amherst.acdc.trellis.vocabulary;
+
+import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.rdf.api.RDF;
+import org.apache.commons.rdf.simple.SimpleRDF;
+
+/**
+ * RDF Terms from the Fedora Ontology
+ *
+ * @see <a href="http://fedora.info/definitions/v4/repository">Fedora Ontology</a>
+ *
+ * @author acoburn
+ */
+public class Fedora {
+
+    private static RDF rdf = new SimpleRDF();
+
+    /* Namespace */
+    public static String uri = "http://fedora.info/definitions/v4/repository#";
+
+    /* Classes */
+    public static IRI AnnotatedResource = rdf.createIRI(uri + "AnnotatedResource");
+    public static IRI Binary = rdf.createIRI(uri + "Binary");
+    public static IRI Configuration = rdf.createIRI(uri + "Configuration");
+    public static IRI Container = rdf.createIRI(uri + "Container");
+    public static IRI EmbedResources = rdf.createIRI(uri + "EmbedResources");
+    public static IRI InboundReferences = rdf.createIRI(uri + "InboundReferences");
+    public static IRI NonRdfSourceDescription = rdf.createIRI(uri + "NonRdfSourceDescription");
+    public static IRI Pairtree = rdf.createIRI(uri + "Pairtree");
+    public static IRI Relations = rdf.createIRI(uri + "Relations");
+    public static IRI RepositoryRoot = rdf.createIRI(uri + "RepositoryRoot");
+    public static IRI Resource = rdf.createIRI(uri + "Resource");
+    public static IRI ServerManaged = rdf.createIRI(uri + "ServerManaged");
+    public static IRI Skolem = rdf.createIRI(uri + "Skolem");
+    public static IRI Thing = rdf.createIRI(uri + "Thing");
+    public static IRI Tombstone = rdf.createIRI(uri + "Tombstone");
+    public static IRI Version = rdf.createIRI(uri + "Version");
+
+    /* Named Individuals */
+    public static IRI inaccessibleResource = rdf.createIRI(uri + "inaccessibleResource");
+
+    /* Object Properties */
+    public static IRI baseVersion = rdf.createIRI(uri + "baseVersion");
+    public static IRI hasAccessRoles = rdf.createIRI(uri + "hasAccessRoles");
+    public static IRI hasFixityService = rdf.createIRI(uri + "hasFixityService");
+    public static IRI hasMember = rdf.createIRI(uri + "hasMember");
+    public static IRI hasNamespaces = rdf.createIRI(uri + "hasNamespaces");
+    public static IRI hasParent = rdf.createIRI(uri + "hasParent");
+    public static IRI hasResultsMember = rdf.createIRI(uri + "hasResultsMember");
+    public static IRI hasVersion = rdf.createIRI(uri + "hasVersion");
+    public static IRI hasVersions = rdf.createIRI(uri + "hasVersions");
+    public static IRI predecessors = rdf.createIRI(uri + "predecessors");
+    public static IRI sparql = rdf.createIRI(uri + "sparql");
+
+    /* Datatype Properties */
+    public static IRI computedChecksum = rdf.createIRI(uri + "computedChecksum");
+    public static IRI computedSize = rdf.createIRI(uri + "computedSize");
+    public static IRI created = rdf.createIRI(uri + "created");
+    public static IRI createdBy = rdf.createIRI(uri + "createdBy");
+    public static IRI hasLocation = rdf.createIRI(uri + "hasLocation");
+    public static IRI hasMoreResults = rdf.createIRI(uri + "hasMoreResults");
+    public static IRI hasTransactionProvider = rdf.createIRI(uri + "hasTransactionProvider");
+    public static IRI hasVersionLabel = rdf.createIRI(uri + "hasVersionLabel");
+    public static IRI isCheckedOut = rdf.createIRI(uri + "isCheckedOut");
+    public static IRI lastModified = rdf.createIRI(uri + "lastModified");
+    public static IRI lastModifiedBy = rdf.createIRI(uri + "lastModifiedBy");
+    public static IRI numberOfChildren = rdf.createIRI(uri + "numberOfChildren");
+    public static IRI numFixityErrors = rdf.createIRI(uri + "numFixityErrors");
+    public static IRI numFixityRepaired = rdf.createIRI(uri + "numFixityRepaired");
+    public static IRI objectCount = rdf.createIRI(uri + "objectCount");
+    public static IRI objectSize = rdf.createIRI(uri + "objectSize");
+    public static IRI writable = rdf.createIRI(uri + "writable");
+
+    private Fedora() {
+        // prevent instantiation
+    }
+}

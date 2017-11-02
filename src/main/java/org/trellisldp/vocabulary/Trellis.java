317e90b30f87f95f30d8f2b7432968f3c27bca46
Aaron Coburn <acoburn@amherst.edu>
2017-11-01 20:22:46 -0400

    Remove recursion term

--- src/main/java/org/trellisldp/vocabulary/Trellis.java
+++ src/main/java/org/trellisldp/vocabulary/Trellis.java
@@ -46,7 +46,6 @@ public final class Trellis extends BaseVocabulary {
     public static final IRI PreferServerManaged = createIRI(URI + "PreferServerManaged");
     public static final IRI PreferUserManaged = createIRI(URI + "PreferUserManaged");
     public static final IRI RepositoryAdministrator = createIRI(URI + "RepositoryAdministrator");
-    public static final IRI UnsupportedRecursiveDelete = createIRI(URI + "UnsupportedRecursiveDelete");
 
     private Trellis() {
         super();
